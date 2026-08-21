3c3b23090fd33effbe0e72c3cededad8a4f4539d
Diego Contreras <diegocontreras401@gmail.com>
2026-08-21 16:20:42 -0500

    finished the sliding transition effect. Im satisfied with it for now, we will see if it stays like that. still need to add some finishing touches

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const panelTwo = document.getElementById('panelTwo');
 const signInForm = document.getElementById('signInForm');
 const signUpForm = document.getElementById('signUpForm');
 const panelOne = document.getElementById('panelOne');
-
+const panelThree = document.getElementById('panelThree'); 
+const panelFour = document.getElementById('panelFour'); 
 
 createAccountBtn.addEventListener('click', () => {
     createAccountContainer.classList.toggle('hidden'); 
@@ -17,9 +18,14 @@ createAccountBtn.addEventListener('click', () => {
     signUpForm.classList.toggle('hidden');
     panelOne.classList.toggle('shiftPanelOne');
     panelOne.classList.toggle('panelOne');
+    panelThree.classList.toggle('shiftPanelThree'); 
+    panelThree.classList.toggle('panelThree'); 
+    panelFour.classList.toggle('shiftPanelFour');
+    panelFour.classList.toggle('panelFour');
+    
 })
 
-alreadyHaveAccountBtn.addEventListener('click', () => {
+alreadyHaveAccountBtn.addEventListener('click', () => { 
     alreadyHaveAccountContainer.classList.toggle('hidden'); 
     createAccountContainer.classList.toggle('hidden'); 
     panelTwo.classList.toggle('shiftPanelTwo'); 
@@ -28,4 +34,8 @@ alreadyHaveAccountBtn.addEventListener('click', () => {
     signUpForm.classList.toggle('hidden');
     panelOne.classList.toggle('shiftPanelOne');
     panelOne.classList.toggle('panelOne');
+    panelThree.classList.toggle('shiftPanelThree'); 
+    panelThree.classList.toggle('panelThree'); 
+    panelFour.classList.toggle('shiftPanelFour');
+    panelFour.classList.toggle('panelFour');
 })
